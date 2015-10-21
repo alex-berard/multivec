d2b08bb7cded45eebd3bc1c53b9bae9abf48c1d1
Alexandre Bérard <eskaev@gmail.com>
2015-10-21 17:55:55 +0200

    Added questions-words.txt

--- benchmarks/analogical_reasoning/word2vecpp.sh
+++ benchmarks/analogical_reasoning/word2vecpp.sh
@@ -3,4 +3,4 @@
 # run from the root directory of the project
 filename=`mktemp`
 bin/word2vecpp --train data/news.en --save-vectors-bin $filename $@
-bin/compute-accuracy $filename 0 < data/questions-words.txt | tail -n3 | head -n2
+bin/compute-accuracy $filename 0 < word2vec/questions-words.txt | tail -n3 | head -n2
